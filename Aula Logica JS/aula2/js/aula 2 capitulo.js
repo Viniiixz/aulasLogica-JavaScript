0895aae9c4006113d9179d59cf6262103d2d9cf6
Carlos Vinícius Souza <carlos.vinicius.dev@gmail.com>
2020-08-20 19:09:03 -0300

    Initial commit

---
+++ Aula Logica JS/aula2/js/aula 2 capitulo.js
@@ -0,0 +1,28 @@
+/* Introdução a eventos e funções
+
+Método
+
+getElementById() ->  esse metodo permite referenciar qualquer elemento da página, como um campo de formulário, um paragrafo, um botão, uma imagem , entre outros.
+
+Propriedades
+
+textContent -> consulta ou alerta o conteúdo de elementos HTML como paragrafos, cabeçalhos ou conteiners.
+innerHTML -> consulta ou altera o conteudo de elementos HTML, como paragrafos cabeçalhos ou conteiners. Códigos HTML 
+presentes no conteúdo são renderizados pelo navegador.
+Value -> consulta ou altera o contéudo de campos de formulário.
+
+Listeners
+
+addEventListener -> utilização dos chamados listeners(ouvintes) de eventos ou modelos de enventos DOM(Document Object Model)
+
+Funções Matemáticas
+
+Math.abs(num) -> retorna um valor absoluto de um numero, ou seja, se o valor for negativo, ele será convertido para positivo.
+Math.cell(num) -> arredonda o valor para cima. Dessa forma, se o valor possuir decimais retorna o proximo numero inteiro do valor analisado.
+Math.floor(num) -> arredonda o valo para baixo, retornando a parte inteira do numero.
+Math.pow(base, exp) -> retorna a base elevada ao expoente.
+Math.random() -> retorna um número aleatório entre 0 e 1, com várias casas decimais. O número aleatório possivel inicia em 0 e vai até um valor inferior 1.
+Math.round(num) -> arredonda o valor para o inteiro mais proximo.
+Math.sqrt(num) -> retorna a raiz quadrada do numero.
+
+*/
